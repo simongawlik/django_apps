811dddd0fd7c165599559e0a6815424ecc5281fc
Simon Gawlik <sgawlik@gmail.com>
2016-01-11 23:41:39 -0500

    updates to names for querysets passed to views

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from models import BlogPost
 
 # Create your views here.
@@ -16,14 +16,16 @@ from models import BlogPost
 def blog_post_create(request):
     return HttpResponse("<h1>Create</h1>")
     
-def blog_post(request, blog_id):
+def blog_post_detail(request, blog_id):
+    instance = get_object_or_404(BlogPost, id=blog_id)
     article = BlogPost.objects.get(pk=blog_id)
-    context = {'article': article}
+    context = {'instance': instance}
     return render(request, "blog_post.html", context)
 
 def blog_list(request):
-    blogs = BlogPost.objects.all()[:4]
-    return render(request, "blog_overview.html", {'blogs': blogs})
+    queryset = BlogPost.objects.all()[:4]
+    context = {'blogpost_list': queryset}
+    return render(request, "blog_overview.html", context)
 
 def blog_post_update(request):
     return HttpResponse("<h1>Update</h1>")
