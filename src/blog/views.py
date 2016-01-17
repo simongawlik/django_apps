1d3c2524bbb1a10a06596fe19039db9531604a0d
Simon Gawlik <sgawlik@gmail.com>
2016-01-17 17:35:37 -0500

    added pagination

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 
@@ -26,9 +27,32 @@ def blog_post_detail(request, blog_id):
 
 
 def blog_list(request):
-    queryset = BlogPost.objects.all()   #.order_by("-created")
+    queryset_list = BlogPost.objects.all()   #.order_by("-created")
+    paginator = Paginator(queryset_list, 3) # Show 3 blog entries per page
+
+    page = request.GET.get('page')
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+    
     context = {'blogpost_list': queryset}
     return render(request, "blog_overview.html", context)
+
+"""
+
+from django.shortcuts import render
+
+def listing(request):
+    contact_list = Contacts.objects.all()
+    
+
+    return render(request, 'list.html', {'contacts': contacts})
+"""
 
 
 def blog_post_update(request, blog_id=None):
